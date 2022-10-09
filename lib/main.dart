3bed88613d0b545c6025333864e0fa84170bec14
TaisoItsukage <taiso.itsukage.dev@gmail.com>
2022-10-09 17:26:54 +0900

    ルートをProviderScopeで囲む

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  //Providerをアプリ内で利用可能にするために範囲（スコープ）を指定
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
